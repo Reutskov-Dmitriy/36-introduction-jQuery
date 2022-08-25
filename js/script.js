2b37dd29aacf034b519d95538dccb6d2914b1b30
Reutskov-Dmitriy <infowelc@gmail.com>
2022-08-25 19:54:10 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ $(function () {
 		$(this).css({ 'color': 'white' })
 	})
 });
+
+// Slider
+let offset = 0; // left offset
+const sliderLine = document.querySelector('.card__slider');
+showSlide()
+
+function offsetRight() {
+	offset += 160;
+	if (offset > 480) offset = 0;
+	sliderLine.style.left = -offset + 'px';
+}
+
+document.querySelector('.card__btnRight').addEventListener('click', offsetRight);
+
+document.querySelector('.card__btnLeft').addEventListener('click', function () {
+	offset -= 160;
+	if (offset < 0) { offset = 480; }
+	sliderLine.style.left = -offset + 'px';
+});
+
+function showSlide() {
+	for (let i = 0; i < 3; i += 1) {
+		offsetRight()
+	}
+}
